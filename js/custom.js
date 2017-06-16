3d50de9db6be061af60a6c82c786336a7aa50662
Rahul <rahulprabhakar0596@gmail.com>
2017-06-16 20:32:04 +0530

    SignOut

--- js/custom.js
+++ js/custom.js
@@ -10,22 +10,27 @@ window.onload = function() {
 };
 
 $(document).ready(function(){
-    $('.navbar-header').hide();
-    $('.navbar-nav').hide();
+    $('#login').hide();
+    $('#signout').hide();
+    $('.navbar-brand').hide();
     var nameBox= $('#nameBox');
     var emailBox= $('#emailBox');
     var phoneBox= $('#phoneBox');
     $("#btn1").click(function(){
-        
         $("#landing").hide();
         $("#signUp").show();
-        $('.navbar-header').show();
-        $('.navbar-nav').show();    
+        $('.navbar-default').css("background","rgba(23, 35, 59, 0.4)");
+        $('.navbar-default').css("box-shadow","-1px 1px 10px 0.2px rgba(0,0,0,0.35)");
+        $('.navbar-default').css("margin-bottom","0");
+        $('#login').show();    
+        $('.navbar-brand').show();        
     })
     $('.signup').click(function(){
         if(nameBox.val().length>0 && emailBox.val().length>0 && phoneBox.val().length>0)
             {
         $('#signUp').hide();
+        $('#signout').show();
+        $('#login').hide();
         if(nameBox.val().length>0 && emailBox.val().length>0)
             {
         $( "div#Username" ).replaceWith( "<h2>"+nameBox.val()+"</h2>" );
@@ -33,13 +38,23 @@ $(document).ready(function(){
         $("div#Phone").append("<h5 style='font-weight:bold'>Phone No</h5><p>"+phoneBox.val()+"</p>")        
             }
         $('#mapScreen').css("visibility","visible");
+        $('.navbar-default').css("background","rgba(23, 35, 59, 1)");
         $('body').css('background','#f4f4f4');
-        $('.navbar-default').css("background","rgba(23, 35, 59, 0.9)");
-        $('.navbar-default').css("box-shadow","-1px 1px 10px 0.2px rgba(0,0,0,0.35)");
-        $('.navbar-default').css("margin-bottom","0");
         $('#myCanvas').css('visibility','hidden');  
             }
             
     })
+    
+    $('#signout').click(function(){
+        $('#signUp').show(); 
+        $(this).hide();
+        $('#login').show();
+        $('#mapScreen').css('visibility','hidden');
+        $('.navbar-default').css("background","rgba(23, 35, 59, 0.4)");
+        $('.navbar-default').css("box-shadow","-1px 1px 10px 0.2px rgba(0,0,0,0.35)");
+        $('.navbar-default').css("margin-bottom","0");
+        $('body').css('background','url("../img/background.jpg") center');
+        $('#myCanvas').css('visibility','visible');  
+    });
 });
 
